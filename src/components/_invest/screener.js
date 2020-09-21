307ae85c05a80e062ef4ab203a43a23544c7a20e
rajkamal3 <rkchenumalla@gmail.com>
2020-09-21 22:43:47 +0530

    local search enabled

--- src/components/_invest/screener.js
+++ src/components/_invest/screener.js
@@ -20,7 +20,33 @@ class Screener extends Component {
         });
     }
 
-    companiesFilter = () => {
+    filterCompanies = () => {
+        var input = document.querySelector('.inputValue').value.toLowerCase();
+        var companiesNamesArray = document.querySelectorAll('.companyName');
+        var companiesSectorArray = document.querySelectorAll('.companySector');
+        var companyRow = document.querySelectorAll('.companyRow');
+
+        for (var i = 0; i < companiesNamesArray.length; i++) {
+            var currentCompany = companiesNamesArray[i];
+            var currentSector = companiesSectorArray[i];
+            var currentCompanyName = currentCompany.textContent || currentCompany.innerText;
+            var currentSectorName = currentSector.textContent || currentSector.innerText;
+
+            if (currentCompanyName.toLowerCase().indexOf(input) > -1 || currentSectorName.toLowerCase().indexOf(input) > -1) {
+                companyRow[i].style.display = '';
+            } else {
+                companyRow[i].style.display = 'none';
+            }
+        }
+    };
+
+    showAllCompanies = () => {
+        Array.from(document.querySelectorAll('.companyRow')).map((el) => {
+            el.style.display = 'grid';
+        });
+    };
+
+    addNewCompany = () => {
         var input = document.querySelector('.inputValue').value.toLowerCase();
 
         if (input.length >= 3) {
@@ -61,6 +87,7 @@ class Screener extends Component {
                             });
                         });
                     document.querySelector('.inputValue').value = 'Added';
+                    this.showAllCompanies();
                     setTimeout(() => {
                         document.querySelector('.inputValue').value = '';
                     }, 3000);
@@ -76,9 +103,7 @@ class Screener extends Component {
         const currentCompanies = this.state.ids;
         const removeCompany = comp.target.getAttribute('data-id');
         const removeCompanyIndex = currentCompanies.indexOf(removeCompany);
-
         currentCompanies.splice(removeCompanyIndex, 1);
-        console.log(currentCompanies);
         this.setState({ ids: currentCompanies });
         axios.put('https://stokr-beta.firebaseio.com/companies/.json', this.state.ids).then((res) => {
             console.log(res);
@@ -94,7 +119,12 @@ class Screener extends Component {
                 <Sidebar showMenu={this.state.showMenu} clicked={this.hideMenu} />
                 <Header clicked={this.hideMenu.bind(this)} />
                 <div className={[styles.searchBar, 'search'].join(' ')}>
-                    <input type="textbox" placeholder="Search for companies..." className={[styles.textbox, 'inputValue'].join(' ')} />
+                    <input
+                        onChange={this.filterCompanies.bind(this)}
+                        type="textbox"
+                        placeholder="Search for companies..."
+                        className={[styles.textbox, 'inputValue'].join(' ')}
+                    />
                     <button
                         style={{
                             height: '100%',
@@ -107,7 +137,7 @@ class Screener extends Component {
                             borderBottomLeftRadius: '0',
                             boxShadow: '0px 0px 30px 0px rgba(10, 9, 15, 0.5)'
                         }}
-                        onClick={this.companiesFilter.bind(this)}
+                        onClick={this.addNewCompany.bind(this)}
                     >
                         Add
                     </button>
@@ -122,7 +152,7 @@ class Screener extends Component {
                     })}
                 </div>
 
-                <div>
+                <div className="companiesContainer">
                     {this.state.ids.length >= 1
                         ? this.state.ids.map((el) => {
                               return <CompanySchema clicked={this.removeCompany} key={el} id={el} />;
